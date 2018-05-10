e02b4555b1eefe9d9d2a19a66690421a36f11c54
Kevin Guo <kevinguo@Kevins-MacBook-Pro-2.local>
2018-05-10 18:33:29 -0400

    fixed form spaces and add a calendar to choose a day

--- newapp/src/index.js
+++ newapp/src/index.js
@@ -1,19 +1,28 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import 'react-day-picker/lib/style.css';
 import './css/index.css';
 import registerServiceWorker from './registerServiceWorker';
-import DayPicker from './daypicker.js';
+import DayPickerInput from './daypicker.js';
 
 class App extends Component{
   constructor(props) {
     super(props);
-
-    this.state = {
-    }
     this.handleDayChange = this.handleDayChange.bind(this);
+    this.state = {
+      selectedDay: undefined,
+    }
+    
+  }
+  
+  handleDayChange(day) {
+    this.setState({ 
+      selectedDay: day 
+    });
   }
 
 render(){
+      const { selectedDay } = this.state;
 return(
 
 		<div className="App">
@@ -21,18 +30,18 @@ return(
 
 
         <nav
-            class="navbar navbar-fixed-top
+            className="navbar navbar-fixed-top
             navbar-default"
             role="navigation"
             >
             
-            <div class="container">
+            <div className="container">
 
 
-            <div class="navbar-header">
+            <div className="navbar-header">
             <button
             type="button"
-            class="navbar-toggle collapsed"
+            className="navbar-toggle collapsed"
             data-toggle="collapse"
             data-target="#navbar_collapse">
             </button>
@@ -40,9 +49,9 @@ return(
 
 
             <div
-            class="collapse navbar-collapse"
+            className="collapse navbar-collapse"
             id="navbar_collapse">
-            <ul class="nav navbar-nav navbar-right">
+            <ul className="nav navbar-nav navbar-right">
             
             
             
@@ -68,10 +77,10 @@ return(
             </div>
             </nav>
 
-            <div class="navbar-placeholder">
+            <div className="navbar-placeholder">
             <div id="content">
-            <div class="mtl mbl lead">
-            <div class="animation-container">
+            <div className="mtl mbl lead">
+            <div className="animation-container">
             <img
             alt="PickUp Banner"
             id="banner"
@@ -82,12 +91,12 @@ return(
             </div>
             </div>
             </div>
-            <div class="feature-block">
-            <div class="container">
-            <div class="row text-center">
-            <div class="col-md-10 col-md-offset-1">
+            <div className="feature-block">
+            <div className="container">
+            <div className="row text-center">
+            <div className="col-md-10 col-md-offset-1">
 
-            <h2 class="feature-header">
+            <h2 className="feature-header">
             <br /><br />
 
             <br /><br />
@@ -98,9 +107,9 @@ return(
             </h2>
             </div>
             </div>
-            <div class="row mtl">
-            <div class="col-md-4 col-md-push-6 lead mbl">
-            <p class="mts">
+            <div className="row mtl">
+            <div className="col-md-4 col-md-push-6 lead mbl">
+            <p className="mts">
             <br /><br />Pickup is a ridesharing service designed to simplify the commute of university students around Ontario and provide the best commuting experience.<br /><br /><br />
             
             Our app is currently in development and will be released in time for early September.<br /><br />
@@ -110,34 +119,36 @@ return(
             </p>
             <p>
             </p>
-            <div class="learn-more-button-wrapper mtm">
+            <div className="learn-more-button-wrapper mtm">
             <a
-            class="btn btn-default btn-bordered"
+            className="btn btn-default btn-bordered"
             target="_blank"
             >
             Sign up to become a driver
             </a>
             </div>
             </div>
-            <div class="col-md-5 col-md-pull-3 mts text-center">
+            <div className="col-md-5 col-md-pull-3 mts text-center">
             
             
-            <div class="animation-container">
+            <div className="animation-container">
             <div id="animation-container">
             <br />
             <br />
-            <iframe src='//www.appdemostore.com/embed?id=4950896824811520' width='334' height='680' frameborder='0'></iframe>
+            <iframe src='//www.appdemostore.com/embed?id=4950896824811520' width='334' height='680' frameBorder='0'></iframe>
             </div>
             </div>
             </div>
             </div>
             </div>
             </div>
-            <div class="feature-block black-bg">
-            <div class="container">
-            <div class="row text-center">
-            <div class="col-md-8 col-md-offset-2">
-            <h2 class="feature-header mtl">
+
+            <div className="feature-block black-bg">
+            <div className="container">
+            <div className="row text-center">
+            <div className="col-md-8 col-md-offset-2">
+
+            <h2 className="feature-header mtl">
             Get A Ride Today <br /> Please Fill Out The Following Form And Select Rider Or Driver<br />
             <br />
             Info:
@@ -145,37 +156,37 @@ return(
             </div>
             </div>
             
-            <div class="row text-center">
+            <div className="row text-center">
 
 
-            <div class="form-group">
-            <label for="RiderForm1">First name:</label>
-            <input type="firstname" class="form-control" id="RiderForm1" placeholder="e.g. John"/>
+            <div className="form-group">
+            <label htmlFor="RiderForm1">First name: </label>
+            <input type="firstname" className="form-control" id="RiderForm1" placeholder="e.g. John"/>
             </div>
 
 
-            <div class="form-group">
-            <label for="RiderForm2">Last name:</label>
-            <input type="lastname" class="form-control" id="RiderForm2" placeholder="e.g. Doe"/>
+            <div className="form-group">
+            <label htmlFor="RiderForm2">Last name: </label>
+            <input type="lastname" className="form-control" id="RiderForm2" placeholder="e.g. Doe"/>
             </div>
 
 
 
-            <div class="form-group">
-            <label for="RiderForm3">Email address:</label>
-            <input type="email" class="form-control" id="RiderForm3" placeholder="name@example.com"/>
+            <div className="form-group">
+            <label htmlFor="RiderForm3">Email address: </label>
+            <input type="email" className="form-control" id="RiderForm3" placeholder="name@example.com"/>
             </div>
 
 
-            <div class="form-group">
-            <label for="RiderForm4">Phone number:</label>
-            <input type="PhoneNumber" class="form-control" id="RiderForm4"/>
+            <div className="form-group">
+            <label htmlFor="RiderForm4">Phone number: </label>
+            <input type="PhoneNumber" className="form-control" id="RiderForm4"/>
             </div>
 
 
-            <div class="form-group">
-            <label for="RiderForm5">Starting From:</label>
-            <select class="form-control" id="RiderForm5">
+            <div className="form-group">
+            <label htmlFor="RiderForm5">Starting From: </label>
+            <select className="form-control" id="RiderForm5">
                   <option>McMaster University</option>
                   <option>Richmond Hill</option>
                   <option>Oakville</option>
@@ -195,9 +206,9 @@ return(
             </div>
 
 
-            <div class="form-group">
-            <label for="RiderForm6">Destination:</label>
-            <select class="form-control" id="RiderForm6">
+            <div className="form-group">
+            <label htmlFor="RiderForm6">Destination: </label>
+            <select className="form-control" id="RiderForm6">
                   <option>McMaster University</option>
                   <option>Richmond Hill</option>
                   <option>Oakville</option>
@@ -215,42 +226,46 @@ return(
                   <option>Kitchener</option>
             </select>
             </div>
-
             <div>
-              {DayPicker}
+              {this.state.selectedDay && <p>Day: {this.state.selectedDay.toLocaleDateString()}</p>}
+              {!this.state.selectedDay && <p>Choose a day</p>}
+              <DayPickerInput 
+                  onDayChange={this.handleDayChange}
+                  dayPickerProps={{
+                        todayButton: 'Today'
+                  }}     
+              />
             </div>
-
-
-            <div class="form-check">
-            <input class="form-check-input" name="radio" type="radio" id="Radios1" value="option1" checked/>
-            <label class="form-check-label" for="Radios1">
+            <div className="form-check">
+            <input className="form-check-input" name="radio" type="radio" id="Radios1" value="option1"/>
+            <label className="form-check-label" htmlFor="Radios1">
                 I am a rider
             </label>
             </div>
 
 
-            <div class="form-check">
-            <input class="form-check-input" name="radio" type="radio" id="Radios2" value="option2"/>
-            <label class="form-check-label" for="Radios2">
+            <div className="form-check">
+            <input className="form-check-input" name="radio" type="radio" id="Radios2" value="option2"/>
+            <label className="form-check-label" htmlFor="Radios2">
                 I am a driver
             </label>
             </div>
 
             <form action="/action_page.php">
-            <button type="submit" class="btn btn-light">Submit</button>
+            <button type="submit" className="btn btn-light">Submit</button>
             </form>
 
-            <div class="feature-block text-center">
-            <div class="container">
-            <h2 class="feature-header">
+            <div className="feature-block text-center">
+            <div className="container">
+            <h2 className="feature-header">
             Contact Us:
             </h2>
             
-            <div class="row mtx mbx">
-            <div class="col-md-10 col-md-offset-1">
+            <div className="row mtx mbx">
+            <div className="col-md-10 col-md-offset-1">
           
 
-            <div class="lead">
+            <div className="lead">
             
             pickuprideshare@gmail.com<br />
             
@@ -275,5 +290,5 @@ return(
 	);
 }
 }
-ReactDOM.render(<App />, <MyForm />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
